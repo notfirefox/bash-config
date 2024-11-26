13fe48ec0194ca50760596f38512e2273a44f064
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-11-26 11:17:37 +0100

    Update config
    Debian and Ubuntu do not use `/etc/bashrc` and `/etc/profile.d/*.sh` to
    set up bash completion and aliases for commands such as ls and grep.
    Instead of relying on `/etc/bashrc` we set those things up manually in
    our `~/.bashrc` file.

--- bashrc.bash
+++ bashrc.bash
@@ -2,11 +2,6 @@
 # shellcheck shell=bash
 # shellcheck disable=SC1091
 
-# source global definitions
-if [[ -f /etc/bashrc ]]; then
-    . /etc/bashrc
-fi
-
 # set environment variables
 export EDITOR=ed
 export VISUAL=vi
@@ -39,5 +34,25 @@ if [[ "$OSTYPE" == darwin* ]]; then
     # use bash completion from homebrew
     if [[ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]]; then
         . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
+    fi
+elif [[ "$OSTYPE" == linux* ]]; then
+    # enable color support of ls and also add handy aliases
+    if [[ -x /usr/bin/dircolors ]]; then
+        eval "$(dircolors -b)"
+        alias ls='ls --color=auto'
+        alias grep='grep --color=auto'
+        alias zgrep='zgrep --color=auto'
+        alias xzgrep='xzgrep --color=auto'
+    fi
+
+    # enable programmable completion features (you don't need to enable
+    # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+    # sources /etc/bash.bashrc).
+    if ! shopt -oq posix; then
+        if [ -f /usr/share/bash-completion/bash_completion ]; then
+            . /usr/share/bash-completion/bash_completion
+        elif [ -f /etc/bash_completion ]; then
+            . /etc/bash_completion
+        fi
     fi
 fi
